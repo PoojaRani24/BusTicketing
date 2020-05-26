4032cf8c0879d03c515d9a84191a8add845d09ae
Pooja Rani <pooja.rani@hashedin.com>
2020-05-27 03:59:41 +0530

    Added /tickets route

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res,next) => {
-    res.status(200).json({
-        message:"It works !"
-    });
-});
+const ticketRoutes = require('./api/routes/tickets');
+
+app.use('/tickets',ticketRoutes);
+
+// app.use((req,res,next) => {
+//     res.status(200).json({
+//         message:"It works !"
+//     });
+// });
 
 module.exports = app;
