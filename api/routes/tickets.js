b818201addf34248f1016e2ec10270decaac1815
Pooja Rani <pooja.rani@hashedin.com>
2020-05-27 15:53:01 +0530

    Added get request routes for open,close,status and details of ticket

--- api/routes/tickets.js
+++ api/routes/tickets.js
@@ -1,44 +1,40 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get('/',(req,res,next) => {
+//----------View Ticket Status---------
+
+router.get('/:ticketId/status',(req,res,next) => {
+    const id=req.params.ticketId;
     res.status(200).json({
-        message : "Handling GET Request to /tickets"
+        message : "Handling GET Request to /tickets/"+id+"/status"
     });
 });
 
-router.post('/',(req,res,next) => {
+//----------View Ticket Details---------
+
+router.get('/:ticketId/details',(req,res,next) => {
+    const id=req.params.ticketId;
     res.status(200).json({
-        message : "Handling POST Request to /tickets"
+        message : "Handling GET Request to /tickets/"+id+"/details"
     });
 });
 
-router.get('/:ticketId',(req,res,next) =>{
-    const id = req.params.ticketId;
-    if(id === 'special'){
-        res.status(200).json({
-            message :'You discovered the special Id',
-            Id:id
-        });
-    }
-    else{
-        res.status(200).json({
-            message :'You passed an Id',
-            Id : id
-        });
-    }
+//----------View Open tickets---------
+
+router.get('/open',(req,res,next) => {
+    res.status(200).json({
+        message : "Handling GET Request to /tickets/open"
+    });
 });
 
-router.patch('/:ticketId',(req,res,next) => {
+//----------View Close tickets---------
+
+router.get('/close',(req,res,next) => {
     res.status(200).json({
-        message:"patch request is served"
-    })
+        message : "Handling GET Request to /tickets/close"
+    });
 });
 
-router.delete('/:ticketId',(req,res,next) => {
-    res.status(200).json({
-        message:"delete request is served"
-    })
-});
+
 
 module.exports = router;
